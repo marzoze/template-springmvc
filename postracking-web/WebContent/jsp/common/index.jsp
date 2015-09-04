54253a5ef9bcc1a05915ee9ec6bf1e4b3b14d133
unknown <FATCHX@IDJKLLAP187.aceins.com>
2015-09-04 17:09:24 +0700

    delete files

--- postracking-web/WebContent/jsp/common/index.jsp
+++
@@ -1,2 +0,0 @@
-<%@ include file="/jsp/common/taglibs.jsp"%>
-<c:redirect url="login"></c:redirect>
--- postracking-web/src/co/id/spring/mvc/sys/config/CommonConstants.java
+++
@@ -1,8 +0,0 @@
-package co.id.spring.mvc.sys.config;
-
-public interface CommonConstants {
-	public static final String JSON = "application/json";
-	public static final String DATE_FORMAT = "dd/MM/yyyy";
-	public static final String DATE_FORMAT_HOUR = "dd/MM/yyyy HH:mm";
-	public static final String DATE_FORMAT_HOUR_12 = "dd/MM/yyyy hh:mm a"; 
-}
